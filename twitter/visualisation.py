c6c941fe21293402583551bcc54814ff6c88ebe8
Aleksandr Bobrov <al.bobrov.mail@gmail.com>
2023-11-26 00:39:01 +0400

    aider: Changed visualization to plot a bar chart with dates on the x-axis and the number of users on the y-axis.
    # Aider chat conversation:
    
    USER: расположи даты по оси x, а по оси y расположи количество пользователей
    
    ASSISTANT: To make the requested changes in the `twitter/visualisation.py` file, we need to modify the `make_frame` function to plot the data with dates on the x-axis and the number of users on the y-axis. We will also need to convert the dates from strings to a format that matplotlib can handle, such as `datetime` objects.
    
    Here are the *SEARCH/REPLACE* blocks to make these changes:
    
    twitter/visualisation.py
    ```python
    <<<<<<< SEARCH
    # Load data from JSON file
    with open('sample_achievements.json', 'r') as file:
        sample_data = json.load(file)
    achievements_data = np.array(list(sample_data.values()))
    
    def make_density_video(filename='output.mp4', fps=30, duration=5):
        # Use the loaded data for visualization
        data = achievements_data
    =======
    import matplotlib.dates as mdates
    from datetime import datetime
    
    # Load data from JSON file
    with open('sample_achievements.json', 'r') as file:
        sample_data = json.load(file)
    # Convert dates from string to datetime objects and sort them
    dates = [datetime.strptime(date, "%Y-%m-%d") for date in sample_data.keys()]
    dates, achievements_data = zip(*sorted(zip(dates, sample_data.values())))
    
    def make_density_video(filename='output.mp4', fps=30, duration=5):
        # Use the loaded data for visualization
        data = np.array(achievements_data)
    >>>>>>> REPLACE
    ```
    
    twitter/visualisation.py
    ```python
    <<<<<<< SEARCH
        def make_frame(t):
            fig, ax = plt.subplots()
            ax.hist(data, bins=30, density=True, alpha=0.75)
            ax.set_xlim(data.min(), data.max())
            # ax.set_ylim(0, None) # This line may not be necessary with hist, but can be adjusted if needed.
            plt.close(fig)
            return mplfig_to_npimage(fig)
    =======
        def make_frame(t):
            fig, ax = plt.subplots()
            ax.bar(dates, data, width=0.8, align='center', alpha=0.75)
            ax.xaxis_date()  # Interpret the x-axis values as dates
            fig.autofmt_xdate()  # Format the dates on the x-axis nicely
            ax.set_ylim(0, max(data) + 10)  # Set y-axis limit
            plt.close(fig)
            return mplfig_to_npimage(fig)
    >>>>>>> REPLACE
    ```
    
    These changes will plot a bar chart with dates on the x-axis and the number of users on the y-axis. The dates are converted to `datetime` objects and sorted, and the `make_frame` function is updated to create a bar chart instead of a histogram. The `mdates` module is used to format the dates on the x-axis.

--- twitter/visualisation.py
+++ twitter/visualisation.py
@@ -5,21 +5,27 @@ from moviepy.editor import VideoClip
 from moviepy.video.io.bindings import mplfig_to_npimage
 import numpy as np
 
+import matplotlib.dates as mdates
+from datetime import datetime
+
 # Load data from JSON file
 with open('sample_achievements.json', 'r') as file:
     sample_data = json.load(file)
-achievements_data = np.array(list(sample_data.values()))
+# Convert dates from string to datetime objects and sort them
+dates = [datetime.strptime(date, "%Y-%m-%d") for date in sample_data.keys()]
+dates, achievements_data = zip(*sorted(zip(dates, sample_data.values())))
 
 def make_density_video(filename='output.mp4', fps=30, duration=5):
     # Use the loaded data for visualization
-    data = achievements_data
+    data = np.array(achievements_data)
     sns.set(style="whitegrid")
 
     def make_frame(t):
         fig, ax = plt.subplots()
-        ax.hist(data, bins=30, density=True, alpha=0.75)
-        ax.set_xlim(data.min(), data.max())
-        # ax.set_ylim(0, None) # This line may not be necessary with hist, but can be adjusted if needed.
+        ax.bar(dates, data, width=0.8, align='center', alpha=0.75)
+        ax.xaxis_date()  # Interpret the x-axis values as dates
+        fig.autofmt_xdate()  # Format the dates on the x-axis nicely
+        ax.set_ylim(0, max(data) + 10)  # Set y-axis limit
         plt.close(fig)
         return mplfig_to_npimage(fig)
 
