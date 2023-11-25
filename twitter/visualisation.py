18bfe3c4a306e548d56ca9562b723a4a60bb04fa
Aleksandr Bobrov <al.bobrov.mail@gmail.com>
2023-11-26 01:09:03 +0400

    aider: Interpolate dates and data for the graph that changes from left to right in `make_density_video` function.
    # Aider chat conversation:
    
    ASSISTANT: Чтобы добавить интерполяцию данных по оси X в функции `make_density_video` в файле `twitter/visualisation.py`, нам нужно будет интерполировать даты, а также данные достижений. Вот шаги, которые мы выполним:
    
    1. Интерполировать массив дат, чтобы соответствовать количеству кадров.
    2. Использовать интерполированные даты при построении графика.
    
    Давайте внесем изменения в файл `twitter/visualisation.py`:
    
    twitter/visualisation.py
    ```python
    <<<<<<< SEARCH
    # Convert dates from string to datetime objects and sort them
    dates = [datetime.strptime(date, "%Y-%m-%d") for date in sample_data.keys()]
    dates, achievements_data = zip(*sorted(zip(dates, sample_data.values())))
    
    def make_density_video(filename='output.mp4', fps=10, duration=5):
        # Use the loaded data for visualization
        data = np.array(achievements_data)
        sns.set(style="whitegrid")
    
        # Calculate the number of frames
        num_frames = duration * fps
        # Create an array to store interpolated data
        interpolated_data = np.zeros((num_frames, len(data)))
    
        # Generate time points for original and interpolated data
        original_time_points = np.linspace(0, 1, len(data))
        interpolated_time_points = np.linspace(0, 1, num_frames)
    
        # Interpolate data for the graph that changes from left to right
        for i, t in enumerate(interpolated_time_points):
            interpolated_data[i] = np.interp(t, original_time_points, data)
    =======
    # Convert dates from string to datetime objects, sort them and interpolate
    dates = [datetime.strptime(date, "%Y-%m-%d") for date in sample_data.keys()]
    dates, achievements_data = zip(*sorted(zip(dates, sample_data.values())))
    # Convert dates to matplotlib date format for interpolation
    mpl_dates = mdates.date2num(dates)
    
    def make_density_video(filename='output.mp4', fps=10, duration=5):
        # Use the loaded data for visualization
        data = np.array(achievements_data)
        sns.set(style="whitegrid")
    
        # Calculate the number of frames
        num_frames = duration * fps
        # Create arrays to store interpolated data
        interpolated_data = np.zeros((num_frames, len(data)))
        interpolated_dates = np.zeros(num_frames)
    
        # Generate time points for original and interpolated data
        original_time_points = np.linspace(0, 1, len(data))
        interpolated_time_points = np.linspace(0, 1, num_frames)
    
        # Interpolate data and dates for the graph that changes from left to right
        for i, t in enumerate(interpolated_time_points):
            interpolated_data[i] = np.interp(t, original_time_points, data)
            interpolated_dates[i] = np.interp(t, original_time_points, mpl_dates)
    >>>>>>> REPLACE
    ```
    
    twitter/visualisation.py
    ```python
    <<<<<<< SEARCH
            ax.plot(dates[:frame_index], interpolated_data[frame_index, :frame_index], marker='', color='purple', linewidth=2)
    =======
            # Convert interpolated dates back to datetime for plotting
            plot_dates = mdates.num2date(interpolated_dates[:frame_index])
            ax.plot(plot_dates, interpolated_data[frame_index, :frame_index], marker='', color='purple', linewidth=2)
    >>>>>>> REPLACE
    ```
    
    Эти изменения добавят интерполяцию дат в соответствии с количеством кадров и будут использовать интерполированные даты при построении графика.

--- twitter/visualisation.py
+++ twitter/visualisation.py
@@ -11,9 +11,11 @@ from datetime import datetime
 # Load data from JSON file
 with open('sample_achievements.json', 'r') as file:
     sample_data = json.load(file)
-# Convert dates from string to datetime objects and sort them
+# Convert dates from string to datetime objects, sort them and interpolate
 dates = [datetime.strptime(date, "%Y-%m-%d") for date in sample_data.keys()]
 dates, achievements_data = zip(*sorted(zip(dates, sample_data.values())))
+# Convert dates to matplotlib date format for interpolation
+mpl_dates = mdates.date2num(dates)
 
 def make_density_video(filename='output.mp4', fps=10, duration=5):
     # Use the loaded data for visualization
@@ -22,21 +24,25 @@ def make_density_video(filename='output.mp4', fps=10, duration=5):
 
     # Calculate the number of frames
     num_frames = duration * fps
-    # Create an array to store interpolated data
+    # Create arrays to store interpolated data
     interpolated_data = np.zeros((num_frames, len(data)))
+    interpolated_dates = np.zeros(num_frames)
 
     # Generate time points for original and interpolated data
     original_time_points = np.linspace(0, 1, len(data))
     interpolated_time_points = np.linspace(0, 1, num_frames)
 
-    # Interpolate data for the graph that changes from left to right
+    # Interpolate data and dates for the graph that changes from left to right
     for i, t in enumerate(interpolated_time_points):
         interpolated_data[i] = np.interp(t, original_time_points, data)
+        interpolated_dates[i] = np.interp(t, original_time_points, mpl_dates)
 
     def make_frame(t):
         frame_index = int(t * fps)
         fig, ax = plt.subplots()
-        ax.plot(dates[:frame_index], interpolated_data[frame_index, :frame_index], marker='', color='purple', linewidth=2)
+        # Convert interpolated dates back to datetime for plotting
+        plot_dates = mdates.num2date(interpolated_dates[:frame_index])
+        ax.plot(plot_dates, interpolated_data[frame_index, :frame_index], marker='', color='purple', linewidth=2)
         # Print data for the current frame
         print(f"Frame {frame_index}: {interpolated_data[frame_index, :frame_index]}")
         ax.xaxis_date()  # Interpret the x-axis values as dates
