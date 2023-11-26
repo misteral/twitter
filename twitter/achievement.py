71357f153110e2eaaa254dfd3dcff70c930951df
Aleksandr Bobrov <al.bobrov.mail@gmail.com>
2023-11-26 11:21:22 +0400

    Added interpolated date

--- twitter/achievement.py
+++ twitter/achievement.py
@@ -3,6 +3,8 @@ from matplotlib.animation import FuncAnimation
 from datetime import datetime, timedelta
 import numpy as np
 import seaborn as sns
+import pandas as pd
+
 
 def visualize_achievement(user_achievements):
     """
@@ -29,15 +31,15 @@ def visualize_achievement(user_achievements):
 # Function to update the plot for each frame
 def update(frame):
     if frame == 0:
-        # plt.plot([], [], color='skyblue', marker='o')  # Initialize an empty plot
-        # plt.xlabel('Date')
-        # plt.ylabel('Number of Users')
+        plt.plot([], [], color='skyblue', marker='o')  # Initialize an empty plot
+        plt.xlabel('Date')
+        plt.ylabel('Number of Users')
         plt.title(f'Twitter Achievements Visualization - {current_date}')
-        plt.xticks(rotation=45)
+        # plt.xticks(rotation=45)
         plt.tight_layout()
         # Set the x and y axis limits
         plt.xlim(np.datetime64(min(dates), 'D') - np.timedelta64(1, 'D'), np.datetime64(max(dates), 'D') + np.timedelta64(1, 'D'))
-        plt.ylim(min(user_counts), max(user_counts))
+        plt.ylim(0, max(user_counts) + 10)
     else:
         # sns.kdeplot(user_counts[:frame], shade=True, color="dodgerblue", label="Cyl=6", alpha=.7)
         plt.plot(dates[:frame], user_counts[:frame], color='skyblue', marker='o')
@@ -47,23 +49,31 @@ current_date = datetime.now().strftime("%Y-%m-%d")
 
 # Example usage:
 if __name__ == '__main__':
-    sample_achievements = {
+    date_users = {
         '2023-11-25': 405,
         '2023-11-23': 386,
         '2023-11-22': 374,
         '2023-11-21': 240,
-        '2023-11-20': 186,
-        '2023-11-17': 150,
-        '2023-11-15': 123,
         '2023-11-12': 99,
     }
-    sns.set_style("white")
+
+    df = pd.DataFrame(list(date_users.items()), columns=['Date', 'Users'])
+    df['Date'] = pd.to_datetime(df['Date'])
+    df.set_index('Date', inplace=True)
+    df.sort_index(inplace=True)
+
+    # Создание пропущенных дат и интерполяция значений
+    df_resampled = df.resample('D').asfreq()  # Создание пропущенных дат
+    df_interpolated = df_resampled.interpolate(method='time')  # Интерполяция значений
+
+    print(df_interpolated)
+
     # Convert string dates to datetime64 for proper sorting and plotting
-    dates = np.array(sorted(sample_achievements.keys()), dtype='datetime64')
-    user_counts = np.array([sample_achievements[str(np.datetime_as_string(date, unit='D'))] for date in dates])
+    # dates = np.array(sorted(sample_achievements.keys()), dtype='datetime64')
+    # user_counts = np.array([sample_achievements[str(np.datetime_as_string(date, unit='D'))] for date in dates])
 
-    fig, ax = plt.subplots(figsize=(10, 5))
+    # fig, ax = plt.subplots(figsize=(10, 5))
 
-    ani = FuncAnimation(fig, update, frames=len(dates), repeat=False)
-    ani.save('twitter_achievement.mp4', writer='ffmpeg', fps=1)
-    plt.show()
+    # ani = FuncAnimation(fig, update, frames=len(dates), repeat=False)
+    # ani.save('twitter_achievement.mp4', writer='ffmpeg', fps=1)
+    # plt.show()
--- twitter/visualisation.py
+++ twitter/visualisation.py
@@ -9,7 +9,7 @@ import matplotlib.dates as mdates
 from datetime import datetime
 
 # Load data from JSON file
-with open('sample_achievements.json', 'r') as file:
+with open('/Users/aleksandrbobrov/data/agents/twitter/twitter/sample_achievements.json', 'r') as file:
     sample_data = json.load(file)
 # Convert dates from string to datetime objects, sort them and interpolate
 dates = [datetime.strptime(date, "%Y-%m-%d") for date in sample_data.keys()]
@@ -17,43 +17,45 @@ dates, achievements_data = zip(*sorted(zip(dates, sample_data.values())))
 # Convert dates to matplotlib date format for interpolation
 mpl_dates = mdates.date2num(dates)
 
-def make_density_video(filename='output.mp4', fps=10, duration=5):
+def make_density_video(filename='output.mp4', fps=2, duration=5):
     # Use the loaded data for visualization
     data = np.array(achievements_data)
     sns.set(style="whitegrid")
 
     # Calculate the number of frames
-    num_frames = duration * fps
-    # Create arrays to store interpolated data
-    interpolated_data = np.zeros((num_frames, len(data)))
-    interpolated_dates = np.zeros(num_frames)
+    # num_frames = duration * fps
+    # # Create arrays to store interpolated data
+    # interpolated_data = np.zeros(num_frames)
+    # interpolated_dates = np.zeros(num_frames)
 
-    # Generate time points for original and interpolated data
-    original_time_points = np.linspace(0, 1, len(data))
-    interpolated_time_points = np.linspace(0, 1, num_frames)
+    # # Generate time points for original and interpolated data
+    # original_time_points = np.linspace(0, 1, len(data))
+    # interpolated_time_points = np.linspace(0, 1, num_frames)
 
-    # Interpolate data and dates for the graph that changes from left to right
-    for i, t in enumerate(interpolated_time_points):
-        interpolated_data[i] = np.interp(t, original_time_points, data)
-        interpolated_dates[i] = np.interp(t, original_time_points, mpl_dates)
+    # # Interpolate data and dates for the graph that changes from left to right
+    # for i, t in enumerate(interpolated_time_points):
+    #     interpolated_data[i] = np.interp(t, original_time_points, data)
+    #     interpolated_dates[i] = np.interp(t, original_time_points, mpl_dates)
 
-    def make_frame(t):
-        frame_index = int(t * fps)
-        fig, ax = plt.subplots()
-        # Convert interpolated dates back to datetime for plotting
-        plot_dates = mdates.num2date(interpolated_dates[:frame_index])
-        # Ensure that the length of plot_dates and the slice of interpolated_data match
-        ax.plot(plot_dates[:frame_index], interpolated_data[frame_index, :frame_index], marker='', color='purple', linewidth=2)
-        # Print data for the current frame
-        print(f"Frame {frame_index}: {interpolated_data[frame_index, :frame_index]}")
-        ax.xaxis_date()  # Interpret the x-axis values as dates
-        fig.autofmt_xdate()  # Format the dates on the x-axis nicely
-        ax.set_xlim(dates[0], dates[-1])  # Set x-axis limit to show all dates
-        ax.set_ylim(0, max(data) + 10)  # Set y-axis limit
-        plt.close(fig)
-        return mplfig_to_npimage(fig)
+    # print(f"numbers: {interpolated_data}")
+    # print(f"dates: {mdates.num2date(interpolated_dates)}")
+    # def make_frame(t):
+    #     frame_index = int(t * fps)
+    #     fig, ax = plt.subplots()
+    #     # Convert interpolated dates back to datetime for plotting
+    #     plot_dates = mdates.num2date(interpolated_dates[:frame_index])
+    #     # Ensure that the length of plot_dates and the slice of interpolated_data match
+    #     ax.plot(plot_dates[:frame_index], interpolated_data[frame_index, :frame_index], marker='', color='purple', linewidth=2)
+    #     # Print data for the current frame
+    #     print(f"Frame {frame_index}: {interpolated_data[frame_index, :frame_index]}")
+    #     ax.xaxis_date()  # Interpret the x-axis values as dates
+    #     fig.autofmt_xdate()  # Format the dates on the x-axis nicely
+    #     ax.set_xlim(dates[0], dates[-1])  # Set x-axis limit to show all dates
+    #     ax.set_ylim(0, max(data) + 10)  # Set y-axis limit
+    #     plt.close(fig)
+    #     return mplfig_to_npimage(fig)
 
-    animation = VideoClip(make_frame, duration=duration)
-    animation.write_videofile(filename, fps=fps)
+    # animation = VideoClip(make_frame, duration=duration)
+    # animation.write_videofile(filename, fps=fps)
 if __name__ == "__main__":
     make_density_video()
