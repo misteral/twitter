8f61868d18c31f3e1bd24889bd06501399a27d90
Aleksandr Bobrov <al.bobrov.mail@gmail.com>
2023-11-26 13:48:36 +0400

    aider: Refactor code to create an animation using `FuncAnimation` and `make_frame` in `twitter/achievement.py`.
    # Aider chat conversation:
    
    ASSISTANT: Чтобы создать покадровую анимацию с использованием функции `FuncAnimation` и `make_frame`, нам нужно внести изменения в файл `twitter/achievement.py`. Вот шаги, которые мы предпримем:
    
    1. Импортировать `FuncAnimation` из `matplotlib.animation`, если это еще не сделано.
    2. Создать функцию `update`, которая будет вызываться на каждом кадре анимации.
    3. Использовать `FuncAnimation` для создания анимации, передавая ей фигуру, функцию `update` и количество кадров.
    4. Сохранить анимацию в файл.
    
    Вот *SEARCH/REPLACE block* для внесения изменений:
    
    twitter/achievement.py
    ```python
    <<<<<<< SEARCH
    # Example usage:
    if __name__ == '__main__':
        # Define current_date at the module level
        current_date = datetime.now().strftime("%Y-%m-%d")
    
        date_users = {
            '2023-11-25': 405,
            '2023-11-23': 386,
            '2023-11-22': 374,
            '2023-11-21': 240,
            '2023-11-12': 99,
        }
    
        df_interpolated = interpolate_data(date_users)
    
        # print(df_interpolated)
    
        # Plotting the interpolated data
        # plt.figure(figsize=(10, 5))
        # plt.plot(df_interpolated.index, df_interpolated['Users'], marker='o')
        # plt.title('Interpolated User Data Over Time')
        # plt.xlabel('Date')
        # plt.ylabel('Number of Users')
        # plt.grid(True)
        # plt.tight_layout()
        # plt.show()
    
        # Convert string dates to datetime64 for proper sorting and plotting
        # dates = np.array(sorted(sample_achievements.keys()), dtype='datetime64')
        # user_counts = np.array([sample_achievements[str(np.datetime_as_string(date, unit='D'))] for date in dates])
    
        fig, ax = plt.subplots(figsize=(10, 5))
    
        # ani = FuncAnimation(fig, update, frames=len(dates), repeat=False)
        # ani.save('twitter_achievement.mp4', writer='ffmpeg', fps=1)
        # plt.show()
    =======
    # Example usage:
    if __name__ == '__main__':
        # Define current_date at the module level
        current_date = datetime.now().strftime("%Y-%m-%d")
    
        date_users = {
            '2023-11-25': 405,
            '2023-11-23': 386,
            '2023-11-22': 374,
            '2023-11-21': 240,
            '2023-11-12': 99,
        }
    
        df_interpolated = interpolate_data(date_users)
    
        # Define the update function for animation
        def update(frame_number):
            plt.clf()  # Clear the current figure
            make_frame(df_interpolated, frame_number)
    
        # Create the figure for the animation
        fig, ax = plt.subplots(figsize=(10, 5))
    
        # Create the animation
        ani = FuncAnimation(fig, update, frames=len(df_interpolated), repeat=False)
        ani.save('twitter_achievement.mp4', writer='ffmpeg', fps=1)
        plt.show()
    >>>>>>> REPLACE
    ```
    
    Этот код создаст анимацию, которая будет показывать график, построенный по данным до текущего кадра, и сохранит ее в виде видеофайла `twitter_achievement.mp4`.

--- twitter/achievement.py
+++ twitter/achievement.py
@@ -52,25 +52,16 @@ if __name__ == '__main__':
 
     df_interpolated = interpolate_data(date_users)
 
-    # print(df_interpolated)
+    # Define the update function for animation
+    def update(frame_number):
+        plt.clf()  # Clear the current figure
+        make_frame(df_interpolated, frame_number)
 
-    # Plotting the interpolated data
-    # plt.figure(figsize=(10, 5))
-    # plt.plot(df_interpolated.index, df_interpolated['Users'], marker='o')
-    # plt.title('Interpolated User Data Over Time')
-    # plt.xlabel('Date')
-    # plt.ylabel('Number of Users')
-    # plt.grid(True)
-    # plt.tight_layout()
-    # plt.show()
-
-    # Convert string dates to datetime64 for proper sorting and plotting
-    # dates = np.array(sorted(sample_achievements.keys()), dtype='datetime64')
-    # user_counts = np.array([sample_achievements[str(np.datetime_as_string(date, unit='D'))] for date in dates])
-
+    # Create the figure for the animation
     fig, ax = plt.subplots(figsize=(10, 5))
 
-    # ani = FuncAnimation(fig, update, frames=len(dates), repeat=False)
-    # ani.save('twitter_achievement.mp4', writer='ffmpeg', fps=1)
-    # plt.show()
+    # Create the animation
+    ani = FuncAnimation(fig, update, frames=len(df_interpolated), repeat=False)
+    ani.save('twitter_achievement.mp4', writer='ffmpeg', fps=1)
+    plt.show()
 
