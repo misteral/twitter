b1110a253eddeb957a3ac46b3055407c102f0053
Aleksandr Bobrov <al.bobrov.mail@gmail.com>
2023-11-26 13:44:33 +0400

    aider: Add function `make_frame` to create a frame for the animation showing the graph up to the given frame number.
    # Aider chat conversation:
    
    ASSISTANT: Для создания функции `make_frame`, которая будет принимать DataFrame `df_interpolated` и номер кадра (индекс), и отображать график от 0 до указанного номера кадра, нам нужно модифицировать файл `twitter/achievement.py`. Вот шаги, которые мы выполним:
    
    1. Определить новую функцию `make_frame` в файле `twitter/achievement.py`.
    2. Функция будет принимать два аргумента: `df_interpolated` и `frame_number`.
    3. Внутри функции, нарисовать график, используя данные от начала до `frame_number`.
    
    Вот *SEARCH/REPLACE block* для добавления новой функции:
    
    twitter/achievement.py
    ```python
    <<<<<<< SEARCH
    =======
    def make_frame(df_interpolated, frame_number):
        """
        Create a frame for the animation showing the graph up to the given frame number.
    
        :param df_interpolated: DataFrame with interpolated user data.
        :param frame_number: Index of the frame to plot up to.
        """
        plt.figure(figsize=(10, 5))
        plt.plot(df_interpolated.index[:frame_number + 1], df_interpolated['Users'][:frame_number + 1], marker='o')
        plt.title('Interpolated User Data Over Time')
        plt.xlabel('Date')
        plt.ylabel('Number of Users')
        plt.grid(True)
        plt.tight_layout()
        return plt
    >>>>>>> REPLACE
    ```
    
    Этот код добавит новую функцию `make_frame` в файл `twitter/achievement.py`. Вы можете использовать эту функцию для создания кадров анимации, передавая ей DataFrame и номер кадра, который вы хотите отобразить.

--- twitter/achievement.py
+++ twitter/achievement.py
@@ -57,3 +57,18 @@ if __name__ == '__main__':
     # ani = FuncAnimation(fig, update, frames=len(dates), repeat=False)
     # ani.save('twitter_achievement.mp4', writer='ffmpeg', fps=1)
     # plt.show()
+def make_frame(df_interpolated, frame_number):
+    """
+    Create a frame for the animation showing the graph up to the given frame number.
+
+    :param df_interpolated: DataFrame with interpolated user data.
+    :param frame_number: Index of the frame to plot up to.
+    """
+    plt.figure(figsize=(10, 5))
+    plt.plot(df_interpolated.index[:frame_number + 1], df_interpolated['Users'][:frame_number + 1], marker='o')
+    plt.title('Interpolated User Data Over Time')
+    plt.xlabel('Date')
+    plt.ylabel('Number of Users')
+    plt.grid(True)
+    plt.tight_layout()
+    return plt
